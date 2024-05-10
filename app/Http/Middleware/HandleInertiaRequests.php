0bca439a93188039a4ce4c92efe774d9733562a1
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2024-05-10 14:07:19 -0600

    make email visible for inertia user prop

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -29,6 +29,8 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $request->user()->makeVisible(['email']);
+
         return [
             ...parent::share($request),
             'auth' => [
