cbf5adaf01c8626040ae7fb97ac1dadb97ca7e4b
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2024-05-13 10:31:56 -0600

    Add CommentFactory and PostLikeFactory for database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Post;
+use App\Models\Comment;
+use App\Models\PostLike;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +16,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()
+        $users = User::factory()
             ->count(5)
             ->hasPosts(3)
             ->create();
+
+        /**
+         * Iterating through all users and creating comments/likes for each post
+         * with a random owner.
+         */
+        for ($i = 0; $i < count($users); $i++) {
+            $user = $users[$i];
+
+            foreach ($user->posts as $post) {
+                $randomUser = $users[rand(0, count($users) - 1)];
+
+                Comment::factory()
+                    ->count(1)
+                    ->for($post)
+                    ->for($randomUser)
+                    ->create();
+
+                $randomPost = $user->posts[rand(0, 2)];
+                PostLike::factory()
+                    ->count(1)
+                    ->for($randomPost)
+                    ->for($randomUser)
+                    ->create();
+            }
+        }
     }
 }
