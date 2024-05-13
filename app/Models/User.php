c181fd9da76b0d73a11c26e8105c9150698bf2b5
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2024-05-13 15:27:14 -0600

    ugrade laravel to 11

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,10 +43,13 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    protected function casts()
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 
     public function posts(): HasMany
     {
